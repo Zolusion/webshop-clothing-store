da07b4faa48e12355506e9bd1eb19116746eea64
Furkan Baykal <furkan.baykal@ara.nl>
2023-12-07 16:28:38 +0100

    latest news in ambassador page

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -9,16 +9,6 @@ interface Post {
     description: string;
 }
 
-const posts = [
-    {
-        id: 1,
-        imageUrl: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        title: "Become an Ambassador",
-        href: "/ambassadors",
-        description: "Are you passionate about our mission? Become an ambassador and make a difference in your community.",
-    }
-]
-
 const Posts = ({ posts }: { posts?: Post[] }) => {
     return (
         <div className="bg-white">
