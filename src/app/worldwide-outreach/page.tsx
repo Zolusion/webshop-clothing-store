0de8b563ed56ad43cf46641e05fe7a65b228a550
Furkan Baykal <furkan.baykal@ara.nl>
2023-12-12 14:51:54 +0100

    new page

---
+++ src/app/worldwide-outreach/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import type { Metadata } from "next";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import Goals from '@/components/Goals';
+
+const title = "Our Goal";
+
+export const metadata: Metadata = {
+    title: `${title} | Solmaz Fashion`,
+}
+
+const page = () => {
+  return (
+    <div className='bg-white'>
+        <Navbar />
+        <Goals />
+        <Footer />
+    </div>
+  )
+}
+
+export default page
