5e5b5a983809cf25df63b6d90a87c1016cda5b2b
Furkan Baykal <furkan.baykal@ara.nl>
2023-12-12 14:51:07 +0100

    deleted some posts

--- src/app/ambassadors/page.tsx
+++ src/app/ambassadors/page.tsx
@@ -21,27 +21,7 @@ const page = () => {
             <AmbassadorInfo />
             <Influencers />
             <Posts posts={[
-                {
-                    id: 1,
-                    imageUrl: "https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aW5zdGFncmFtfGVufDB8fDB8fHww",
-                    title: "Instagram Ambassador",
-                    href: "#",
-                    description: "Instagram is a great social media platform for fashion enthusiasts. We believe that fashion should be accessible to everyone, and we are committed to making that a reality.",
-                },
-                {
-                    id: 2,
-                    imageUrl: "https://images.unsplash.com/photo-1611162618071-b39a2ec055fb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZWJvb2t8ZW58MHx8MHx8fDA%3D",
-                    title: "Facebook Ambassador",
-                    href: "#",
-                    description: "Facebook is great for content creation. Use your own platform to share our fashion with the world.",
-                },
-                {
-                    id: 3,
-                    imageUrl: "https://images.unsplash.com/photo-1611605698323-b1e99cfd37ea?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGlrdG9rfGVufDB8fDB8fHww",
-                    title: "Tiktok Ambassador",
-                    href: "#",
-                    description: "Tiktok is lately the fastest growing social media platform. Like, Comment, and Share.",
-                },
+
                 {
                     id: 4,
                     imageUrl: "/fashiontrends.jpeg",
