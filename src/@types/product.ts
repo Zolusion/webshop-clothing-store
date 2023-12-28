16d0fd11f22a10b73ea00e26ad64aca5dc210322
Furkan Baykal <furkan.baykal@ara.nl>
2023-12-28 08:32:48 +0100

    redux

---
+++ src/@types/product.ts
@@ -0,0 +1,10 @@
+export default interface Product {
+    id?: string;
+    productName: string;
+    description: string;
+    imageUrl: string;
+    quantity: number;
+    oldPrice?: string;
+    newPrice?: string;
+
+}
---
+++ src/store/cart/cartSlice.ts
@@ -0,0 +1,48 @@
+'use client';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import Product from '@/@types/product';
+
+const persistedCartItems = localStorage.getItem('cartItems');
+const initialState: any = {
+  items: persistedCartItems ? JSON.parse(persistedCartItems) : [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<Product>) => {
+      const { id } = action.payload;
+      const existingItem = state.items.find((item: any) => item.id === id);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        let cartnewItem = { ...action.payload, quantity: 1 };
+        if(action.payload.newPrice){
+            cartnewItem = {
+                id: cartnewItem.id,
+                productName: cartnewItem.productName,
+                description: cartnewItem.description,
+                imageUrl: cartnewItem.imageUrl,
+                quantity: 25,
+                oldPrice: cartnewItem.oldPrice,
+                newPrice: cartnewItem.newPrice
+            }
+        }
+        state.items.push(cartnewItem);
+      }
+      localStorage.setItem('cartItems', JSON.stringify(state.items));
+    },
+    removeItem: (state, action: PayloadAction<string>) => {
+      state.items = state.items.filter((item: any) => item.id !== action.payload);
+      localStorage.setItem('cartItems', JSON.stringify(state.items));
+    },
+    clearCart: state => {
+      state.items = [];
+      localStorage.removeItem('cartItems');
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
